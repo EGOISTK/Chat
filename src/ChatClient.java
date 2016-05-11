8b5e5cba3f6ccc5808968fc0ffdea392e8cdb23a
EGOIST.K <chinazhjjq@qq.com>
2016-05-11 16:59:37 +0800

    version 0.2

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,6 +1,10 @@
 import java.awt.*;
 
 public class ChatClient extends Frame {
+
+    TextField tf = new TextField();
+    TextArea ta = new TextArea();
+
     public static void main(String[] args) {
         new ChatClient().launchFrame();
     }
@@ -8,6 +12,9 @@ public class ChatClient extends Frame {
     public void launchFrame() {
         setLocation(400, 150);
         setSize(500, 500);
+        add(ta, BorderLayout.NORTH);
+        add(tf, BorderLayout.SOUTH);
+        pack();
         setVisible(true);
     }
 }
