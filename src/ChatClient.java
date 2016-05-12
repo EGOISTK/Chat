70a9183d7c8c05da0a5612cfd63edbec30ca7956
EGOIST.K <chinazhjjq@qq.com>
2016-05-12 10:54:25 +0800

    version 0.6

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,7 +1,11 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
-import java.net.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class ChatClient extends Frame {
 
@@ -13,77 +17,119 @@ public class ChatClient extends Frame {
     private TextArea ta = new TextArea();
 
     public static void main(String[] args) {
+
         new ChatClient("ChatClient").launchFrame();
+
     }
 
     private ChatClient(String s) {
+
         super(s);
+
     }
 
     private void launchFrame() {
+
         setLocation(400, 150);
         setSize(500, 500);
         add(launchName,BorderLayout.NORTH);
         add(launch, BorderLayout.SOUTH);
         pack();
+
         launch.addActionListener(new LaunchListener());
+
         this.addWindowListener(new WindowAdapter() {
+
             @Override
             public void windowClosing(WindowEvent e) {
+
                 System.exit(0);
+
             }
+
         });
+
         tf.addActionListener(new TFListener());
+
         setVisible(true);
         setResizable(true);
         connect();
+
     }
 
     private void connect() {
+
         try {
+
             s = new Socket("127.0.0.1", 8888);
+
         } catch (IOException e) {
+
             e.printStackTrace();
+
         }
+
     }
 
     private class LaunchListener implements ActionListener {
+
         @Override
         public void actionPerformed(ActionEvent e) {
+
             if (!launchName.getText().equals("")) {
+
                 nickName = launchName.getText();
+
                 try {
+
                     DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
                     dos.writeUTF(nickName + " is launched!");
                     dos.flush();
                     dos.close();
+
                 } catch (IOException e1) {
+
                     e1.printStackTrace();
+
                 }
+
                 remove(launchName);
                 remove(launch);
                 add(ta, BorderLayout.NORTH);
                 add(tf, BorderLayout.SOUTH);
                 pack();
+
             }
+
         }
+
     }
 
     private class TFListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+
             String str = tf.getText().trim();
+
             ta.setText(str);
             tf.setText("");
+
             try {
+
                 DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
                 dos.writeUTF(str);
                 dos.flush();
                 dos.close();
+
             } catch (IOException e1) {
+
                 e1.printStackTrace();
+
             }
+
         }
 
     }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,20 +1,32 @@
-import java.io.*;
-import java.net.*;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ChatServer {
 
     public static void main(String[] args) {
 
         try {
+
             ServerSocket ss = new ServerSocket(8888);
+
             while (true) {
+
                 Socket s = ss.accept();
+
                 DataInputStream dis = new DataInputStream(s.getInputStream());
+
                 System.out.println(dis.readUTF());
+
             }
+
         } catch (IOException e) {
+
             e.printStackTrace();
+
         }
+
     }
 
 }
