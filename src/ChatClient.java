8cdac4660bcb50c11ff4ea8ff5d8b7b39fe0e82e
EGOIST.K <chinazhjjq@qq.com>
2016-05-12 18:56:42 +0800

    version 0.8

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,4 +1,8 @@
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.TextArea;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
